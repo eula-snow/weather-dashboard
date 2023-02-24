28c6b43655da2b996d70d177170f7c7b2b07474e
Maria Konova <arim.novak@gmail.com>
2023-02-24 18:45:21 +0000

    saves cities to localStorage

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ var apiKey = "77d4eb011b9ab8b90b0c6086ad369f1b";
 var searchButton = $(".search-button");
 var searchInput = $("#search-input");
 var today = $("#today");
+var forecast = $("#forecast");
+var history = $("#history");
 
 // var queryURL =
 //   "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
@@ -11,16 +13,25 @@ var today = $("#today");
 function search(event) {
   event.preventDefault();
   var city = searchInput.val();
+
+  console.log("search: ");
+  console.log(city);
+  updateSearchHistory(city);
+  displaySearchHistory();
+
   getWeather(city);
 }
 
 //function to display current weather
 function displayCurrentWeather(currentObj) {
-  $("#current").hide();
+  today.html("");
   today.append(`
         <div>
-        <h3>${currentObj.name}</h3>
-        <p>Temp: ${currentObj.main.temp}</p>
+        <h3>${currentObj.name} <img src='${
+    "https://openweathermap.org/img/w/" + currentObj.weather[0].icon
+  }.png'></h3>
+        
+        <p>Temp: ${currentObj.main.temp} °C</p>
         <p>Wind: ${currentObj.wind.speed} KPH</p>
         <p>Humidity: ${currentObj.main.humidity}%</p>
 </div>`);
@@ -37,4 +48,32 @@ function getWeather(city) {
   });
 }
 
+//function that adds city to localStorage
+function updateSearchHistory(city) {
+  // get json string containing the array of cities
+  var citiesStr = localStorage.getItem("cities");
+  var citiesArr = citiesStr != null ? JSON.parse(citiesStr) : [];
+  if (citiesArr.includes(city)) {
+    return false;
+  }
+  citiesArr.push(city); //adds new value to the array
+  localStorage.setItem("cities", JSON.stringify(citiesArr)); //converts array in string and adds to localStorage
+  return true;
+}
+
+//function that displays search history as buttons
+function displaySearchHistory() {
+  $("#history").html("");
+
+  var citiesStr = localStorage.getItem("cities");
+  var citiesArr = citiesStr != null ? JSON.parse(citiesStr) : [];
+
+  for (const city of citiesArr) {
+    $("#history").append(`
+  <button class='btn citySearch' onClick=getWeather('${city}') >${city}</button>
+  `);
+  }
+}
+
 searchButton.click(search);
+displaySearchHistory();
